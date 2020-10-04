e627415e985e0af124f35020fadca6b264b1eac0
rahulsai143 <kandularahulreddy96@gmail.com>
2020-10-04 15:20:49 +0530

    [NOJIRATICKET] zuulproxygateway server creation

---
+++ src/test/java/com/couponzz/couponsfactory/zuulproxygateway/ZuulproxygatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponzz.couponsfactory.zuulproxygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulproxygatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
